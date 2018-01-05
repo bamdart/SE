2f93be2f6076fb942479c1ee5d8ae09a9f1d4dea
An-Jie Shih <james41009@gmail.com>
2018-01-04 18:21:00 -0600

    v7
    剛剛版本號打錯

--- SE/Form1.Designer.cs
+++ SE/Form1.Designer.cs
@@ -31,13 +31,20 @@
             this.components = new System.ComponentModel.Container();
             this.pictureBox1 = new System.Windows.Forms.PictureBox();
             this.button1 = new System.Windows.Forms.Button();
-            this.textBox1 = new System.Windows.Forms.TextBox();
             this.button2 = new System.Windows.Forms.Button();
             this.timer1 = new System.Windows.Forms.Timer(this.components);
             this.button3 = new System.Windows.Forms.Button();
             this.button4 = new System.Windows.Forms.Button();
             this.label1 = new System.Windows.Forms.Label();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.pictureBox3 = new System.Windows.Forms.PictureBox();
+            this.pictureBox4 = new System.Windows.Forms.PictureBox();
+            this.pictureBox5 = new System.Windows.Forms.PictureBox();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox5)).BeginInit();
             this.SuspendLayout();
             // 
             // pictureBox1
@@ -50,33 +57,25 @@
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(870, 12);
+            this.button1.Enabled = false;
+            this.button1.Location = new System.Drawing.Point(764, 69);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(75, 23);
-            this.button1.TabIndex = 2;
+            this.button1.TabIndex = 0;
             this.button1.TabStop = false;
             this.button1.Text = "開始遊戲";
             this.button1.UseVisualStyleBackColor = true;
-            this.button1.Click += new System.EventHandler(this.button1_Click);
-            // 
-            // textBox1
-            // 
-            this.textBox1.Enabled = false;
-            this.textBox1.Location = new System.Drawing.Point(685, 136);
-            this.textBox1.Multiline = true;
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(175, 348);
-            this.textBox1.TabIndex = 3;
             // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(879, 459);
+            this.button2.Enabled = false;
+            this.button2.Location = new System.Drawing.Point(764, 293);
             this.button2.Name = "button2";
             this.button2.Size = new System.Drawing.Size(66, 23);
             this.button2.TabIndex = 4;
+            this.button2.TabStop = false;
             this.button2.Text = "離開遊戲";
             this.button2.UseVisualStyleBackColor = true;
-            this.button2.Click += new System.EventHandler(this.button2_Click);
             // 
             // timer1
             // 
@@ -85,23 +84,25 @@
             // 
             // button3
             // 
-            this.button3.Location = new System.Drawing.Point(870, 242);
+            this.button3.Enabled = false;
+            this.button3.Location = new System.Drawing.Point(764, 210);
             this.button3.Name = "button3";
             this.button3.Size = new System.Drawing.Size(75, 23);
             this.button3.TabIndex = 5;
+            this.button3.TabStop = false;
             this.button3.Text = "暫停遊戲";
             this.button3.UseVisualStyleBackColor = true;
-            this.button3.Click += new System.EventHandler(this.button3_Click);
             // 
             // button4
             // 
-            this.button4.Location = new System.Drawing.Point(870, 117);
+            this.button4.Enabled = false;
+            this.button4.Location = new System.Drawing.Point(764, 136);
             this.button4.Name = "button4";
             this.button4.Size = new System.Drawing.Size(75, 23);
             this.button4.TabIndex = 6;
+            this.button4.TabStop = false;
             this.button4.Text = "繼續遊戲";
             this.button4.UseVisualStyleBackColor = true;
-            this.button4.Click += new System.EventHandler(this.button4_Click);
             // 
             // label1
             // 
@@ -112,16 +113,59 @@
             this.label1.TabIndex = 7;
             this.label1.Text = "label1";
             // 
+            // pictureBox2
+            // 
+            this.pictureBox2.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.pictureBox2.Location = new System.Drawing.Point(845, 52);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(100, 50);
+            this.pictureBox2.TabIndex = 8;
+            this.pictureBox2.TabStop = false;
+            this.pictureBox2.Click += new System.EventHandler(this.pictureBox2_Click);
+            // 
+            // pictureBox3
+            // 
+            this.pictureBox3.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.pictureBox3.Location = new System.Drawing.Point(845, 119);
+            this.pictureBox3.Name = "pictureBox3";
+            this.pictureBox3.Size = new System.Drawing.Size(100, 50);
+            this.pictureBox3.TabIndex = 9;
+            this.pictureBox3.TabStop = false;
+            this.pictureBox3.Click += new System.EventHandler(this.pictureBox3_Click);
+            // 
+            // pictureBox4
+            // 
+            this.pictureBox4.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.pictureBox4.Location = new System.Drawing.Point(845, 196);
+            this.pictureBox4.Name = "pictureBox4";
+            this.pictureBox4.Size = new System.Drawing.Size(100, 50);
+            this.pictureBox4.TabIndex = 10;
+            this.pictureBox4.TabStop = false;
+            this.pictureBox4.Click += new System.EventHandler(this.pictureBox4_Click);
+            // 
+            // pictureBox5
+            // 
+            this.pictureBox5.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.pictureBox5.Location = new System.Drawing.Point(845, 266);
+            this.pictureBox5.Name = "pictureBox5";
+            this.pictureBox5.Size = new System.Drawing.Size(100, 50);
+            this.pictureBox5.TabIndex = 11;
+            this.pictureBox5.TabStop = false;
+            this.pictureBox5.Click += new System.EventHandler(this.pictureBox5_Click);
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(957, 798);
+            this.Controls.Add(this.pictureBox5);
+            this.Controls.Add(this.pictureBox4);
+            this.Controls.Add(this.pictureBox3);
+            this.Controls.Add(this.pictureBox2);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.button4);
             this.Controls.Add(this.button3);
             this.Controls.Add(this.button2);
-            this.Controls.Add(this.textBox1);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.pictureBox1);
             this.KeyPreview = true;
@@ -129,6 +173,10 @@
             this.Text = "Form1";
             this.Load += new System.EventHandler(this.Form1_Load);
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox5)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -137,12 +185,15 @@
         #endregion
         private System.Windows.Forms.PictureBox pictureBox1;
         private System.Windows.Forms.Button button1;
-        private System.Windows.Forms.TextBox textBox1;
         private System.Windows.Forms.Button button2;
         private System.Windows.Forms.Timer timer1;
         private System.Windows.Forms.Button button3;
         private System.Windows.Forms.Button button4;
         private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.PictureBox pictureBox3;
+        private System.Windows.Forms.PictureBox pictureBox4;
+        private System.Windows.Forms.PictureBox pictureBox5;
     }
 }
 
--- SE/Form1.cs
+++ SE/Form1.cs
@@ -15,78 +15,68 @@ namespace SE
     {
         List<List<List<Point>>> cubeShape = new List<List<List<Point>>>();//各方塊的初始形狀
 
-        Point nowCube = new Point(7, 0);//現在方塊所在位置
-        int[] nowShape = { 0, 0 };
+        Point nowPoint = new Point(7, 0);//現在方塊所在位置
+        int[] nowShape = { 0, 0 };//現在的圖形 , 現在圖形是第幾旋轉形狀
 
-        Pen pen = new Pen(Color.Black, 1);//格線
+        Pen pen = new Pen(Color.Black, 1);//格線顏色 , 粗度
 
-        int screenWidth = 15;
-        int screenHeigh = 24;
-        int cubeWidth = 20;
+        int gameWidth = 10; //寬有幾格
+        int gameHeigh = 20; //高有幾格
+        int cubeWidth = 30; //格子寬度
 
         SolidBrush[] Brush = {
-        new SolidBrush(Color.Empty),
-        new SolidBrush(Color.Red),
+        new SolidBrush(Color.LightGray),//空白格子的顏色
+        new SolidBrush(Color.Red),//其他方塊的顏色
         new SolidBrush(Color.Green),
         new SolidBrush(Color.Blue),
-        new SolidBrush(Color.Black),
-        new SolidBrush(Color.Gray),
+        new SolidBrush(Color.DarkRed),
+        new SolidBrush(Color.Yellow),
         new SolidBrush(Color.Purple)};
 
         BufferedGraphicsContext bufferedGraphicsContext;//buffer
-        BufferedGraphics graphics;
+        BufferedGraphics graphics;//畫圖用
 
-        List<List<int>> gameScreen = new List<List<int>>();
-        //List<List<int>> gameScreenColor = new List<List<int>>();
-        Random random = new Random();
+        List<List<int>> gameScreen = new List<List<int>>();//畫面現在有的方塊 0~7 0是空白 其他是各種方塊
+        Random random = new Random();//用來隨機產生方塊種類
 
-        List<List<Rectangle>> rect = new List<List<Rectangle>>();//rect位置
+        List<List<Rectangle>> rect = new List<List<Rectangle>>();//rect位置，畫圖用
 
-        public int score = 0;
+        public int score = 0;//分數
 
-        public int GameSpeed = 100; 
+        public int gameSpeed = 500;//遊戲速度 
 
-        public void ClearNowShapeFromScreen()
+        public void ClearNowShapeFromScreen()//把現有方塊從畫面上先消除，避免影響判斷碰撞
         {
             for (int i = 0; i < 4; i++)
             {
-                int tempX = nowCube.X + cubeShape[nowShape[0]][nowShape[1]][i].X;
-                int tempY = nowCube.Y + cubeShape[nowShape[0]][nowShape[1]][i].Y;
+                int tempX = nowPoint.X + cubeShape[nowShape[0]][nowShape[1]][i].X;
+                int tempY = nowPoint.Y + cubeShape[nowShape[0]][nowShape[1]][i].Y;
                 gameScreen[tempY][tempX] = 0;
             }
         }
 
-        public void AddShapeToScreen()
+        public void AddShapeToScreen()//將現有方塊加入畫面中
         {
             for (int i = 0; i < 4; i++)
             {
-                int tempX = nowCube.X + cubeShape[nowShape[0]][nowShape[1]][i].X;
-                int tempY = nowCube.Y + cubeShape[nowShape[0]][nowShape[1]][i].Y;
-                gameScreen[tempY][tempX] = nowShape[0] + 1;
+                int tempX = nowPoint.X + cubeShape[nowShape[0]][nowShape[1]][i].X;
+                int tempY = nowPoint.Y + cubeShape[nowShape[0]][nowShape[1]][i].Y;
+                gameScreen[tempY][tempX] = nowShape[0] + 1;//從1~7 0被用來當空白，所以+1
             }
         }
 
-        /*public void FocusCube()
-        {
-            for (int i = 0; i < 4; i++)
-            {
-                int tempX = nowCube.X + cubeShape[nowShape[0]][nowShape[1]][i].X;
-                int tempY = nowCube.Y + cubeShape[nowShape[0]][nowShape[1]][i].Y;
-                gameScreen[tempY][tempX] = 2;
-            }
-        }*/
-
-        public int CheckBound(Point cube, int[] shape)//0是正常 1是超出寬度 2是超出高度 3是碰到方塊
+        //0是正常 1是超出寬度 2是超出高度 3是碰到方塊
+        public int CheckBound(Point cube, int[] shape)
         {
             for (int i = 0; i < 4; i++)
             {
                 int tempX = cube.X + cubeShape[shape[0]][shape[1]][i].X;
                 int tempY = cube.Y + cubeShape[shape[0]][shape[1]][i].Y;
-                if (tempX < 0 || tempX >= screenWidth)//超出寬度
+                if (tempX < 0 || tempX >= gameWidth)//超出寬度
                 {
                     return 1;
                 }
-                if (tempY < -2 || tempY >= screenHeigh)//超出高度
+                if (tempY < 0 || tempY >= gameHeigh)//超出高度
                 {
                     return 2;
                 }
@@ -95,7 +85,7 @@ namespace SE
             {
                 int tempX = cube.X + cubeShape[shape[0]][shape[1]][i].X;
                 int tempY = cube.Y + cubeShape[shape[0]][shape[1]][i].Y;
-                if (gameScreen[tempY][tempX] != 0  )//0是空的
+                if (gameScreen[tempY][tempX] != 0 )
                 {
                     return 3;
                 }
@@ -104,13 +94,14 @@ namespace SE
             return 0;
         }
 
-        public void Rotate()
+        public void Rotate()//轉動
         {
             int[] tempShape = new int[2];
             tempShape[0] = nowShape[0];
             tempShape[1] = nowShape[1];
 
-            if (cubeShape[tempShape[0]].Count <= tempShape[1] + 1)//下一個形狀長怎樣
+            //下一個形狀長怎樣
+            if (cubeShape[tempShape[0]].Count <= tempShape[1] + 1)
             {
                 tempShape[1] = 0;
             }
@@ -118,152 +109,164 @@ namespace SE
             {
                 tempShape[1]++;
             }
+
             ClearNowShapeFromScreen();
 
-            if (CheckBound(nowCube, tempShape) != 0)
+            //如果有碰撞，直接畫原本的回去畫面
+            if (CheckBound(nowPoint, tempShape) != 0)
             {
                 AddShapeToScreen();
                 return;
             }
 
+            //新的位置
             nowShape = tempShape;
-
             AddShapeToScreen();
         }
 
+        //往左
         public void GoLeft()
         {
             Point tempCube = new Point();
-            tempCube = nowCube;
-            tempCube.X = nowCube.X - 1;
+            tempCube = nowPoint;
+            tempCube.X = nowPoint.X - 1;
 
             ClearNowShapeFromScreen();
 
+            //如果有碰撞，直接畫原本的回去畫面
             if (CheckBound(tempCube, nowShape) != 0)
             {
                 AddShapeToScreen();
                 return;
             }
 
-            nowCube = tempCube;
-
+            nowPoint = tempCube;
             AddShapeToScreen();
         }
 
         public void GoRight()
         {
             Point tempCube = new Point();
-            tempCube = nowCube;
-            tempCube.X = nowCube.X + 1;
+            tempCube = nowPoint;
+            tempCube.X = nowPoint.X + 1;
 
             ClearNowShapeFromScreen();
 
+            //如果有碰撞，直接畫原本的回去畫面
             if (CheckBound(tempCube, nowShape) != 0)
             {
                 AddShapeToScreen();
                 return;
             }
 
-            nowCube = tempCube;
-
+            nowPoint = tempCube;
             AddShapeToScreen();
         }
 
         public void GoDown()
         {
-            Point tempCube = new Point();
-            tempCube = nowCube;
+            Point tempPoint = new Point();
+            tempPoint = nowPoint;
+            tempPoint.Y = tempPoint.Y + 1;
+
             ClearNowShapeFromScreen();
 
-            while (CheckBound(tempCube, nowShape) == 0)
+            //死亡判斷
+            if ((CheckBound(tempPoint, nowShape) == 2 || CheckBound(tempPoint, nowShape) == 3) && nowPoint.Y == 0)
             {
-                tempCube.Y = tempCube.Y + 1;
-                ClearNowShapeFromScreen();
-                if (CheckBound(tempCube, nowShape) == 2 || CheckBound(tempCube, nowShape) == 3)
-                {
-                    tempCube.Y = tempCube.Y - 1;
-                    ClearNowShapeFromScreen();
-
-                    nowCube = tempCube;
-
-                    AddShapeToScreen();
-
-                    CheckClearRow();
-
-                    AddShapeToScreen();
-
-                    break;
-                }
+                timer1.Enabled = false;//遊戲停止
+                AddShapeToScreen();
+                return;
             }
-            AddShapeToScreen();
 
-
-        }
-
-        public void TimerDown()
-        {
-            Point tempCube = new Point();
-            tempCube = nowCube;
-            tempCube.Y = nowCube.Y + 1;
-            ClearNowShapeFromScreen();
-
-            if (CheckBound(tempCube, nowShape) == 2 || CheckBound(tempCube, nowShape) == 3)
+            //下降碰撞判斷，有碰到
+            if (CheckBound(tempPoint, nowShape) == 2 || CheckBound(tempPoint, nowShape) == 3)
             {
-                tempCube.Y = tempCube.Y - 1;
+                tempPoint.Y = tempPoint.Y - 1;//往回
 
-                nowCube = tempCube;
-
+                nowPoint = tempPoint;
                 AddShapeToScreen();
 
                 CheckClearRow();
-
                 AddShapeToScreen();
 
                 return;
             }
 
-            ClearNowShapeFromScreen();
-
-            nowCube = tempCube;
-
+            nowPoint = tempPoint;
             AddShapeToScreen();
         }
 
-        public bool CheckRowAllIsCube(int row)
+        //下到底部
+        public void DownToBottom()
+        { 
+            Point tempPoint = new Point();
+            tempPoint = nowPoint;
+            ClearNowShapeFromScreen();
+
+            if ((CheckBound(tempPoint, nowShape) == 2 || CheckBound(tempPoint, nowShape) == 3) && nowPoint.Y == 0)
+            {
+                timer1.Enabled = false;//遊戲停止
+                AddShapeToScreen();
+                return;
+            }
+
+            while (CheckBound(tempPoint, nowShape) == 0)
+            {
+                tempPoint.Y = tempPoint.Y + 1;
+                if (CheckBound(tempPoint, nowShape) == 2 || CheckBound(tempPoint, nowShape) == 3)
+                {
+                    tempPoint.Y = tempPoint.Y - 1;
+
+                    nowPoint = tempPoint;
+                    AddShapeToScreen();
+
+                    CheckClearRow();
+                    AddShapeToScreen();
+
+                    break;
+                }
+            }
+        }
+
+        public bool CheckRowAllIsCube(int row)//確定整排都有東西
         {
-            for (int i = 0; i < screenWidth; i++)
+            for (int i = 0; i < gameWidth; i++)
                 if (gameScreen[row][i] == 0 )
                     return false;
             return true;
         }
 
+        //確定有沒有消除
         public void CheckClearRow()
         {
-            //FocusCube();
-
-            for (int i = nowCube.Y; i < screenHeigh; i++)
+            for (int i = nowPoint.Y; i < gameHeigh; i++)
             {
-                if (CheckRowAllIsCube(i))
+                if (CheckRowAllIsCube(i))//如果整排被填滿
                 {
-                    score++;
-                    label1.Text = score.ToString();
-                    gameScreen.RemoveAt(i);
+                    score++;//加分
+                    label1.Text = score.ToString();//分數刷新
+
+                    //可以考慮在這邊加消除特效
+
+                    gameScreen.RemoveAt(i);//刪除被選取的那行
+
                     List<int> tempScreen = new List<int>();
                     for (int j = 0; j < rect[i].Count; j++)
                     {
                         tempScreen.Add(0);
                     }
-                    gameScreen.Insert(0, tempScreen);
+                    gameScreen.Insert(0, tempScreen);//插回最上層
                 }
             }
 
-            CreateNewCube();
+            CreateNewCube();//新方塊
         }
 
         public void CreateNewCube()
         {
-            nowCube.X = screenWidth / 2;
-            nowCube.Y = 0;
+            nowPoint.X = gameWidth / 2;
+            nowPoint.Y = 0;
 
             nowShape[0] = random.Next(0, 6);
             nowShape[1] = 0;
@@ -272,64 +275,57 @@ namespace SE
         public Form1()
         {
             InitializeComponent();
-            pictureBox1.Height = cubeWidth * screenHeigh + 1;
-            pictureBox1.Width = cubeWidth * screenWidth + 1;
+            pictureBox1.Height = cubeWidth * gameHeigh + 1;//設定遊戲視窗大小
+            pictureBox1.Width = cubeWidth * gameWidth + 1;
 
+            //畫圖用
             bufferedGraphicsContext = BufferedGraphicsManager.Current;
             graphics = bufferedGraphicsContext.Allocate(pictureBox1.CreateGraphics(), pictureBox1.DisplayRectangle);
 
-            this.KeyPreview = true;
+            //按鍵事件
             KeyDown += new KeyEventHandler(From1_KeyDown);
-
         }
-
-
 
         private void Form1_Load(object sender, EventArgs e)
         {
-            InitCubeShape();//所有方塊的形狀
+            InitCubeShape();//所有方塊的形狀先產生好
 
             CreateNewCube();//產生一個新方塊
 
-            for (int i = 0; i < cubeWidth * screenHeigh; i += cubeWidth)//rect位置產生
+            for (int i = 0; i < cubeWidth * gameHeigh; i += cubeWidth) //rect位置初始化
             {
                 List<Rectangle> tempRect = new List<Rectangle>();
-                for (int j = 0; j < cubeWidth * screenWidth; j += cubeWidth)
+                for (int j = 0; j < cubeWidth * gameWidth; j += cubeWidth)
                 {
                     tempRect.Add(new Rectangle(j, i, cubeWidth, cubeWidth));
                 }
                 rect.Add(tempRect);
             }
 
-            for (int i = 0; i < screenHeigh; i++)//
+            for (int i = 0; i < gameHeigh; i++)//畫面數據初始化
             {
                 List<int> tempScreen = new List<int>();
-                //List<int> tempScreen1 = new List<int>();
-                for (int j = 0; j < screenWidth; j++)
+                for (int j = 0; j < gameWidth; j++)
                 {
                     tempScreen.Add(0);
-                    //tempScreen1.Add(0);
                 }
                 gameScreen.Add(tempScreen);
-                //gameScreenColor.Add(tempScreen1);
             }
 
-            timer1.Interval = GameSpeed;
+            timer1.Interval = gameSpeed;//設定遊戲速度
 
             AddShapeToScreen();
             drawComponent(gameScreen);
-
         }
 
         public void drawComponent(List<List<int>> list)
         {
-            graphics.Graphics.Clear(Color.White);
+            graphics.Graphics.Clear(Color.White);//清除，底色
 
-            for (int i = 0; i < screenHeigh; i++)
+            for (int i = 0; i < gameHeigh; i++)//畫圖
             {
-                for (int j = 0; j < screenWidth; j++)
+                for (int j = 0; j < gameWidth; j++)
                 {
-
                     graphics.Graphics.FillRectangle(Brush[list[i][j]], rect[i][j]);//填滿顏色
                     graphics.Graphics.DrawRectangle(pen, rect[i][j]);//格線
                 }
@@ -344,83 +340,37 @@ namespace SE
             Console.WriteLine(e.KeyCode);
             if (timer1.Enabled == true)
             {
-                if (e.KeyCode == Keys.W)
+                if (e.KeyCode == Keys.Up)
                 {
-                    textBox1.Text += e.KeyCode;
-
                     Rotate();
                     drawComponent(gameScreen);
                 }
-                if (e.KeyCode == Keys.S)
+                if (e.KeyCode == Keys.Down)
                 {
-                    textBox1.Text += e.KeyCode;
-
                     GoDown();
                     drawComponent(gameScreen);
                 }
-                if (e.KeyCode == Keys.A)
+                if (e.KeyCode == Keys.Left)
                 {
-                    textBox1.Text += e.KeyCode;
-
                     GoLeft();
                     drawComponent(gameScreen);
                 }
-                if (e.KeyCode == Keys.D)
+                if (e.KeyCode == Keys.Right)
                 {
-                    textBox1.Text += e.KeyCode;
-
                     GoRight();
+                    drawComponent(gameScreen);
+                }
+                if(e.KeyCode == Keys.Space)
+                {
+                    DownToBottom();
                     drawComponent(gameScreen);
                 }
             }
         }
 
-        //開始遊戲
-        private void button1_Click(object sender, EventArgs e)
-        {
-            for (int i = 0; i < screenHeigh; i++)//初始化畫面
-            {
-                for (int j = 0; j < screenWidth; j++)
-                {
-                    gameScreen[i][j] = 0;
-                }
-            }
-
-            timer1.Enabled = true;//timer1 開始動作
-
-            score = 0;
-            label1.Text = score.ToString();
-
-            CreateNewCube();
-
-            AddShapeToScreen();
-
-            drawComponent(gameScreen);
-        }
-
-
-        //離開遊戲按鈕
-        private void button2_Click(object sender, EventArgs e)
-        {
-            this.Close();
-        }
-
-        //暫停遊戲
-        private void button3_Click(object sender, EventArgs e)
-        {
-            timer1.Enabled = false;
-        }
-
-        //繼續遊戲
-        private void button4_Click(object sender, EventArgs e)
-        {
-            timer1.Enabled = true;
-        }
-
-        //時鐘
         private void timer1_Tick(object sender, EventArgs e)
         {
-            TimerDown();
+            GoDown();//隨時間下降
             drawComponent(gameScreen);
         }
 
@@ -555,6 +505,43 @@ namespace SE
             cubeShape[6].Add(tempShape);
         }
 
+        //開始遊戲
+        private void pictureBox2_Click(object sender, EventArgs e)
+        {
+            for (int i = 0; i < gameHeigh; i++)//初始化畫面
+            {
+                for (int j = 0; j < gameWidth; j++)
+                {
+                    gameScreen[i][j] = 0;
+                }
+            }
 
+            timer1.Enabled = true;//timer1 開始動作
+
+            score = 0;
+            label1.Text = score.ToString();
+
+            CreateNewCube();
+            AddShapeToScreen();
+
+            drawComponent(gameScreen);
+        }
+        //繼續遊戲
+        private void pictureBox3_Click(object sender, EventArgs e)
+        {
+            timer1.Enabled = true;
+        }
+
+        //暫停遊戲
+        private void pictureBox4_Click(object sender, EventArgs e)
+        {
+            timer1.Enabled = false;
+        }
+
+        //離開遊戲
+        private void pictureBox5_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
     }
 }
